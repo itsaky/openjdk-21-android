ca8ee008aee505622c3c1d10ce4738a921694d11
Akash Yadav <itsaky01@gmail.com>
2023-09-16 15:14:08 +0530

    fix: workaround for tagged pointers on Android

--- src/java.base/share/native/libjli/java.c
+++ src/java.base/share/native/libjli/java.c
@@ -56,6 +56,32 @@
 #include "java.h"
 #include "jni.h"
 
+#ifdef __ANDROID__
+#include <stdbool.h>
+#include <dlfcn.h>
+static void android_disable_tags() {
+    void *lib_handle = dlopen("libc.so", RTLD_LAZY);
+    if (lib_handle) {
+        if (android_get_device_api_level() >= 31) {
+            int (*mallopt_func)(int, int) = dlsym(lib_handle, "mallopt");
+            if (mallopt_func) {
+                mallopt_func(M_BIONIC_SET_HEAP_TAGGING_LEVEL, 0);
+            }
+            return;
+        }
+        /* android_get_device_api_level() < 31 */
+        bool (*android_mallopt)(int opcode, void* arg, size_t arg_size) = dlsym(lib_handle, "android_mallopt");
+        if (android_mallopt) {
+            int android_malloc_tag_level = 0;
+            android_mallopt(8, &android_malloc_tag_level, sizeof(android_malloc_tag_level));
+        }
+        dlclose(lib_handle);
+    }
+}
+#else
+static void android_disable_tags(){}
+#endif
+
 /*
  * A NOTE TO DEVELOPERS: For performance reasons it is important that
  * the program image remain relatively small until after SelectVersion
@@ -252,6 +278,8 @@ JLI_Launch(int argc, char ** argv,              /* main argc, argv */
     _program_name = pname;
     _is_java_args = javaargs;
     _wc_enabled = cpwildcard;
+
+    android_disable_tags();
 
     InitLauncher(javaw);
     DumpState();
